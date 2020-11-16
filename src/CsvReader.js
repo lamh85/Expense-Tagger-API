95c446d0606a44a51492978dfed098276c5acafe
Hon Lam <hon@junglescout.com>
2020-11-15 16:10:48 -0800

    Identify the column that has the vendor's name.

--- src/CsvReader.js
+++ src/CsvReader.js
@@ -1,9 +1,48 @@
 import path from 'path'
 import fs from 'fs'
 
-const csvPath = path.resolve('csv_input', 'pc_financial.csv')
-const csvBuffer = fs.readFileSync(csvPath)
-const csvString = csvBuffer.toString()
-console.log(csvString)
+const getFileString = () => {
+  const csvPath = path.resolve('csv_input', 'pc_financial.csv')
+  const csvBuffer = fs.readFileSync(csvPath)
+  return csvBuffer.toString()
+}
 
-export const csvArray = csvString.split(/\r\n|\n/).map(row => row.split(','))
+export const parseCells = row => row.split(',')
+
+export const getCsvArray = () => {
+  const fileString = getFileString()
+  return fileString.split(/\r\n|\n/).map(parseCells)
+}
+
+const vendorFieldCommonTerms = [
+  'description'
+]
+
+const arrayToRegex = array => {
+  const piped = array.join('|')
+  return new RegExp(piped, 'gi')
+}
+
+export const getVendorIndex = csvArray => {
+  const headerRow = csvArray[0]
+
+  // console.log('headerRow ===============')
+  // console.log(headerRow)
+  // const cells = headerRow.map(parseCells)
+  // console.log('cells =================')
+  // console.log(cells)
+
+  const foundIndex = headerRow.findIndex(cell => {
+    console.log('=============')
+    console.log(cell)
+    console.log(cell.toUpperCase())
+    const pattern = arrayToRegex(vendorFieldCommonTerms)
+    return pattern.test(cell)
+  })
+
+  console.log('foundIndex ===============')
+  console.log(foundIndex)
+
+  if (foundIndex > 0) return foundIndex
+  return null
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-import { csvArray } from './CsvReader.js'
+import {
+  getCsvArray,
+  getVendorIndex
+} from './CsvReader.js'
 import { findCategory } from './CategoryFinder.js'
+
+const csvArray = getCsvArray()
 
 const csvArrayTagged = csvArray.map((row, rowIndex) => {
   if (rowIndex == 0) return row
 
-  const columnsWithLetters = row.filter(column => /[A-Za-z]/.test(column))
+  // const columnsWithLetters = row.filter(column => /[A-Za-z]/.test(column))
 
-  let matchedCategory = null
+  // let matchedCategory = null
 
-  for (let column of columnsWithLetters) {
-    // Don't check the other columns if we already match a keyword
-    if (matchedCategory != null) break
-    matchedCategory = findCategory(column)
-  }
+  const vendorIndex = getVendorIndex(csvArray)
+
+  const vendor = row[vendorIndex]
+  const matchedCategory = findCategory(vendor)
+
+  // for (let column of columnsWithLetters) {
+  //   // Don't check the other columns if we already match a keyword
+  //   if (matchedCategory != null) break
+  //   matchedCategory = findCategory(column)
+  // }
 
   return [...row, matchedCategory]
 })
