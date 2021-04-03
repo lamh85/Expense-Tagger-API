702b192cb22e7221491039b41604c3b0d76ae75c
lamh85 <lamh85@gmail.com>
2021-04-03 14:36:05 -0700

    Remove comments.

--- src/CsvReader.js
+++ src/CsvReader.js
@@ -26,24 +26,12 @@ const arrayToRegex = array => {
 export const getVendorIndex = csvArray => {
   const headerRow = csvArray[0]
 
-  // console.log('headerRow ===============')
-  // console.log(headerRow)
-  // const cells = headerRow.map(parseCells)
-  // console.log('cells =================')
-  // console.log(cells)
-
   const foundIndex = headerRow.findIndex(cell => {
-    console.log('=============')
-    console.log(cell)
-    console.log(cell.toUpperCase())
     const pattern = arrayToRegex(vendorFieldCommonTerms)
     return pattern.test(cell)
   })
 
-  console.log('foundIndex ===============')
-  console.log(foundIndex)
-
   if (foundIndex >= 0) return foundIndex
-  console.log('did not find index')
+
   return null
 }
