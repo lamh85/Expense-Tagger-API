7d8a86aba42128d0a8bfcdedee684d80e9e60c7c
lamh85 <lamh85@gmail.com>
2021-10-13 20:48:40 -0700

    Statement downloader: go straight to login page.

--- src/statement_downloaders/pc_financial.js
+++ src/statement_downloaders/pc_financial.js
@@ -1,13 +1,17 @@
+import dotenv from 'dotenv'
 import Puppeteer from "puppeteer"
+
+dotenv.config()
 
 const getStatement = async () => {
   const browser = await Puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto('https://www.pcfinancial.ca/en/', {
-    waitUntil: 'load',
-  })
 
-  return page
+  await page.setUserAgent('HeadlessChrome')
+  page.goto('https://secure.pcfinancial.ca/en/login')
+  await page.waitForSelector('input#username')
+
+  return page.$eval('html', html => html.outerHTML)
 }
 
 export default getStatement
