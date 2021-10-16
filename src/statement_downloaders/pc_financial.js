dbe74411845a37edb87ca82e4aab520838e0f282
lamh85 <lamh85@gmail.com>
2021-10-16 14:57:27 -0700

    Investigation: Puppeteer page does not load after log into PC Financial.

--- src/statement_downloaders/pc_financial.js
+++ src/statement_downloaders/pc_financial.js
@@ -3,13 +3,52 @@ import Puppeteer from "puppeteer"
 
 dotenv.config()
 
+const CSS_SELECTORS = {
+  USERNAME_FIELD: 'input#username',
+  PASSWORD_FIELD: 'input#password',
+  LOGIN_BUTTON: 'button[type="submit"]',
+  ACCOUNT_SUMMARY_LINKS: '.account-summary a'
+}
+
 const getStatement = async () => {
   const browser = await Puppeteer.launch()
   const page = await browser.newPage()
 
+  page.on('response', async response => {
+    if (response._request._url !== 'https://app.pcfinancial.ca/inet/banking/v2.0/iam/login') return
+    console.log('response --------------')
+    // const json = await response.json()
+    // console.log(json)
+    response.json().then(json => console.log(json))
+
+    // if (request._url !== 'https://app.pcfinancial.ca/inet/banking/v2.0/iam/login') return
+    // console.log('request --------------')
+    // console.log(request)
+  })
+
   await page.setUserAgent('HeadlessChrome')
   page.goto('https://secure.pcfinancial.ca/en/login')
-  await page.waitForSelector('input#username')
+  await page.waitForSelector(CSS_SELECTORS.USERNAME_FIELD, { visible: true })
+
+  await page.type(CSS_SELECTORS.USERNAME_FIELD, process.env.PC_FINANCIAL_USERNAME)
+  await page.type(CSS_SELECTORS.PASSWORD_FIELD, process.env.PC_FINANCIAL_PASSWORD)
+  await page.click(CSS_SELECTORS.LOGIN_BUTTON)
+  console.log('clicked login --------------')
+
+  // could not find selector .account-summary
+
+  // DEBUG BLOCK
+  // await page.waitForTimeout(5000)
+  // const html = await page.$eval('html', html => html.outerHTML)
+  // console.log(html)
+
+  await page.waitForSelector('.account-summary', { visible: true })
+
+  const accountSummaryLinks = document.querySelectorAll(CSS_SELECTORS.ACCOUNT_SUMMARY_LINKS)
+  const txLinkIndex = [...accountSummaryLinks].findIndex(e => (/view transactions/i).test(e.innerText))
+  const txLinkHumanIndex = txLinkIndex + 1
+
+  await page.click(`${CSS_SELECTORS.ACCOUNT_SUMMARY_LINKS}:nth-child(${txLinkHumanIndex})`)
 
   return page.$eval('html', html => html.outerHTML)
 }
